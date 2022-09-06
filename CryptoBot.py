422edcbe2a6dd4d8c6abd3aa01a378081f7c8855
KGeri201 <36195747+kgerg@users.noreply.github.com>
2022-09-06 23:25:05 +0200

    added service

--- CryptoBot.py
+++ CryptoBot.py
@@ -1,3 +1,7 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import time
 import yaml
 import logging
 from binance.client import Client
@@ -6,6 +10,7 @@ api_key = None
 api_secret = None
 
 currency = "EUR"
+time_to_wait = 1
 logging_level = 3
 sell_threshold = 0
 trade_threshold = 0
@@ -35,32 +40,34 @@ def findBestPriceChangePercentage(client):
 def trade(client, asset):
     best_asset = findBestPriceChangePercentage(client)
     if float(best_asset["priceChangePercent"]) < sell_threshold:
+        #order = client.create_test_order(
+        #    symbol=asset["asset"]+currency,
+        #    side="SELL",
+        #    type="MARKET",
+        #    quantity=asset["free"])
+        #print(order)
         pass
     else:
         print(best_asset)
 
 def main():
-    client = Client(api_key, api_secret, testnet=True)
-    #client = Client(api_key, api_secret)
+    #client = Client(api_key, api_secret, testnet=True)
+    client = Client(api_key, api_secret)
     #order = client.order_market_sell(
     #symbol='ETHEUR')
     #print(order)
+    #while True:
     assets = getBalance(client.get_account())
     for asset in assets:
         print(asset)
-        order = client.create_test_order(
-            symbol=asset["asset"]+currency,
-            side="SELL",
-            type="MARKET",
-            quantity=asset["free"])
-        print(order)
         try:
             ticker = client.get_ticker(symbol=(asset["asset"]+currency))
             print(str(float(ticker["priceChangePercent"]))+" %")
             if float(ticker["priceChangePercent"]) < trade_threshold:
                 trade(client, asset)
         except:
-            pass
+            print("No ticker with the current currency was found")
+        #time.sleep(time_to_wait * 3600)
 
  
 if __name__ == "__main__":
@@ -71,16 +78,22 @@ if __name__ == "__main__":
     logger = logging.getLogger()
     try:
         config = yaml.load(open(r'config.yaml'), Loader=yaml.FullLoader)
-        #api_key = config["api_key"]
-        #api_secret = config["api_secret"]
-        api_key = config["api_key_test"]
-        api_secret = config["api_secret_test"]
+        api_key = config["api_key"]
+        api_secret = config["api_secret"]
+        #api_key = config["api_key_test"]
+        #api_secret = config["api_secret_test"]
     except:
-        pass
+        msg = "No config file was found!"
+        logger.warning(msg)
+        print(msg)
     try:
-        if api_key is None or api_secret is None:
+        if (api_key is None or api_secret is None) :
             raise Exception("api_key or api_secret is not set")
         main()
+    except KeyboardInterrupt:
+        msg = "CryptoBot was manually terminated"
+        logger.warning(msg)
+        print(msg)
     except Exception as e:
         logger.error(e)
         print(e)
